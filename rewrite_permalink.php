081bd1f66667dd002e449e2d2ac9d41db0b3510c
Ánh Kevin <anh_kevin@hotmail.com>
2019-07-22 11:25:51 +0700

    Update rewrite_permalink.php

--- rewrite_permalink.php
+++ rewrite_permalink.php
@@ -9,6 +9,7 @@ function eg_add_rewrite_rules() {
     global $wp_rewrite;
  
     $new_rules = array(
+		// Add param type in category taste
         'category-taste/(.+)/([^/]+)' => 'index.php?category-taste='.$wp_rewrite->preg_index(2).'&type='.$wp_rewrite->preg_index(1).''
 	);
     $wp_rewrite->rules = $new_rules + $wp_rewrite->rules;
