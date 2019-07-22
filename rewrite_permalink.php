c8ddaf09342fd51c793df8684d5cf53f39e962ba
Ánh Kevin <anh_kevin@hotmail.com>
2019-07-22 11:24:47 +0700

    Create rewrite_permalink.php

---
+++ rewrite_permalink.php
@@ -0,0 +1,16 @@
+/**
+ * Function: eg_add_rewrite_rules
+ * Change url by new rules
+ *
+ * @return string
+ * @author tien_anh
+ */
+function eg_add_rewrite_rules() {
+    global $wp_rewrite;
+ 
+    $new_rules = array(
+        'category-taste/(.+)/([^/]+)' => 'index.php?category-taste='.$wp_rewrite->preg_index(2).'&type='.$wp_rewrite->preg_index(1).''
+	);
+    $wp_rewrite->rules = $new_rules + $wp_rewrite->rules;
+}
+add_action( 'generate_rewrite_rules', 'eg_add_rewrite_rules' );
