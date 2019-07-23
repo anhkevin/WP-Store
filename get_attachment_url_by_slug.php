b6e0551f1f0a23c8560200fdc437f954ea238c29
Ánh Kevin <anh_kevin@hotmail.com>
2019-07-23 08:19:01 +0700

    Create get_attachment_url_by_slug.php
    Get attachment featured url by name

---
+++ get_attachment_url_by_slug.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Function: get_attachment_url_by_slug
+ * Get attachment featured url by name
+ *
+ * @return string
+ * @author tien_anh
+ */
+function get_attachment_url_by_slug( $slug ) {
+	$args = array(
+	  'post_type' => 'attachment',
+	  'name' => sanitize_title($slug),
+	  'posts_per_page' => 1,
+	  'post_status' => 'inherit',
+	);
+	$_header = get_posts( $args );
+	$header = $_header ? array_pop($_header) : null;
+	return $header ? wp_get_attachment_url($header->ID) : '';
+}
