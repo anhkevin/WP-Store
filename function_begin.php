13ce23fc81a54498b81cd235b92cb110ddd6e02c
Ánh Kevin <anh_kevin@hotmail.com>
2019-07-25 16:16:17 +0700

    Create function_begin.php

---
+++ function_begin.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Theme ABC functions and definitions
+ *
+ * @package WordPress
+ * @author tien_anh - yyyy/mm/dd
+ * @since 1.0
+ * @version 1.0
+ */
+
+define( 'VERSION_JQUERY', '1.1' );
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function theme_option_setup() {
+	/*
+	 * Make theme available for translation.
+	 */
+	load_theme_textdomain( 'themeABC' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+	// Thumbnail image
+	add_image_size( 'thumbnail-post-240-160', 240, 160, true );
+
+	// This theme uses wp_nav_menu() in two locations.
+	register_nav_menus(
+		array(
+			'top'    => __( 'Top Menu', 'themetosmo' ),
+			'footer_property' => __( '物件を探す', 'themetosmo' ),
+			'footer_service' => __( 'サービス', 'themetosmo' ),
+			'footer_company' => __( 'カンパニー', 'themetosmo' ),
+			'media_menu' => __( 'Media Menu', 'themetosmo' ),
+		)
+	);
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support(
+		'html5',
+		array(
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		)
+	);
+
+	/*
+	 * Enable support for Post Formats.
+	 *
+	 * See: https://codex.wordpress.org/Post_Formats
+	 */
+	add_theme_support(
+		'post-formats',
+		array(
+			'aside',
+			'image',
+			'video',
+			'quote',
+			'link',
+			'gallery',
+			'audio',
+		)
+	);
+
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	// Load regular editor styles into the new block-based editor.
+	add_theme_support( 'editor-styles' );
+
+	// Load default block styles.
+	add_theme_support( 'wp-block-styles' );
+
+	// Add support for responsive embeds.
+	add_theme_support( 'responsive-embeds' );
+
+	// Define and register starter content to showcase the theme on new sites.
+	$starter_content = array(
+		// Set up nav menus for each of the two areas registered in the theme.
+		'nav_menus'   => array(
+			// Assign a menu to the "top" location.
+			'top'    => array(
+				'name'  => __( 'Top Menu', 'themetosmo' )
+			),
+
+			// Assign a menu to the "social" location.
+			'footer' => array(
+				'name'  => __( 'Footer Links', 'themetosmo' )
+			),
+		),
+	);
+
+	/**
+	 * Filters Theme array of starter content.
+	 *
+	 * @since Theme 
+   1.1
+	 *
+	 * @param array $starter_content Array of starter content.
+	 */
+	$starter_content = apply_filters( 'theme_starter_content', $starter_content );
+
+	add_theme_support( 'starter-content', $starter_content );
+}
+add_action( 'after_setup_theme', 'theme_option_setup' );
+
