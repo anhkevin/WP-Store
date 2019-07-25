7b09900a634054f7511076bf022d1ae060bab018
Ánh Kevin <anh_kevin@hotmail.com>
2019-07-25 15:34:00 +0700

    Create get_url_width_paramter.php
    Get url current with parameter

---
+++ get_url_width_paramter.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Get url current with parameter
+ *
+ * @return string
+ * @author tien_anh
+ */
+function get_url_width_paramter() {
+	global $wp;
+  	return add_query_arg( $_SERVER['QUERY_STRING'], '', home_url( $wp->request ) );
+}
