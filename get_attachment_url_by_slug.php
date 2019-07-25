5cd0fdb3c3f4c9917db5473728f8ac5f08c294ba
Ánh Kevin <anh_kevin@hotmail.com>
2019-07-25 15:35:59 +0700

    Update get_attachment_url_by_slug.php

--- get_attachment_url_by_slug.php
+++ get_attachment_url_by_slug.php
@@ -7,14 +7,16 @@
  * @return string
  * @author tien_anh
  */
-function get_attachment_url_by_slug( $slug ) {
-	$args = array(
-	  'post_type' => 'attachment',
-	  'name' => sanitize_title($slug),
-	  'posts_per_page' => 1,
-	  'post_status' => 'inherit',
-	);
-	$_header = get_posts( $args );
-	$header = $_header ? array_pop($_header) : null;
-	return $header ? wp_get_attachment_url($header->ID) : '';
+if ( ! function_exists( 'get_attachment_url_by_slug' ) ) {
+	function get_attachment_url_by_slug( $slug ) {
+		$args = array(
+		  'post_type' => 'attachment',
+		  'name' => sanitize_title($slug),
+		  'posts_per_page' => 1,
+		  'post_status' => 'inherit',
+		);
+		$_header = get_posts( $args );
+		$header = $_header ? array_pop($_header) : null;
+		return $header ? wp_get_attachment_url($header->ID) : '';
+	}
 }
