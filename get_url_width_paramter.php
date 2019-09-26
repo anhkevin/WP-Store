c91f3eb89d03c31f9064e074a6a79f96e7db7a56
Ánh Kevin <anh_kevin@hotmail.com>
2019-09-26 08:43:40 +0700

    Update get_url_width_paramter.php

--- get_url_width_paramter.php
+++ get_url_width_paramter.php
@@ -6,7 +6,7 @@
  * @return string
  * @author tien_anh
  */
-if ( ! function_exists( 'get_url_width_paramter' ) ) {
+if ( ! function_exists( 'get_url_with_paramter' ) ) {
 	function get_url_width_paramter() {
 		global $wp;
 		return add_query_arg( $_SERVER['QUERY_STRING'], '', home_url( $wp->request ) );
